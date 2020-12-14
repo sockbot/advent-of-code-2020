40ea3dbb561dbefc89f91f2b64247ca99b9e59be
Andy Chan <github@sockbot.com>
2020-12-13 21:38:19 -0800

    solve part 1

---
+++ 14.py
@@ -0,0 +1,21 @@
+lines = """
+"""
+lines = lines.strip()
+lines = [line for line in lines.split("\n")]
+
+
+from helpers import timer
+
+
+# @timer
+def part1(lines):
+    return
+
+
+# @timer
+def part2(lines):
+    return
+
+
+print(f"Answer 1: {part1(lines)}")
+print(f"Answer 2: {part2(lines)}")
